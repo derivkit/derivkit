6dc38ca243536cffb4af2a9f6ecd2c9e24411730
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-09-27 17:58:41 -0400

    tests(#78): 1:1 test files for forecasting/{expansions,calculus,likelihoods}; moved LE tests

---
+++ tests/test_calculus.py
@@ -0,0 +1,9 @@
+"""Unit tests for forecasting/calculus.py."""
+
+import pytest
+
+
+@pytest.mark.skip(reason="TODO(#78): add unit tests for forecasting/calculus.py")
+def test_calculus_todo():
+    """TODO(#78): add unit tests for forecasting/calculus.py."""
+    pass
---
+++ tests/test_likelihoods.py
@@ -0,0 +1,9 @@
+"""Unit tests for forecasting/likelihoods.py."""
+
+import pytest
+
+
+@pytest.mark.skip(reason="TODO(#78): add unit tests for forecasting/likelihoods.py")
+def test_likelihoods_todo():
+    """TODO(#78): add unit tests for forecasting/likelihoods.py."""
+    pass
