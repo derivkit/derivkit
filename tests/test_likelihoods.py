046bcd0ec3acbc3ebf72755d52ebaf4d413cc96f
Matthijs van der Wild <primarius@gmail.com>
2025-10-19 14:12:12 +0100

    Fix test function names
    Some of the function names were mixed up, so the functions didn't
    actually describe the thing they were testing.

--- tests/test_likelihoods.py
+++ tests/test_likelihoods.py
@@ -172,7 +172,7 @@ def test_gaussian_likelihood():
   ]
 )
 
-def test_poissonian_likelihood_negative_parameters(
+def test_poissonian_likelihood_output(
     input_data,
     input_parameters,
     expected_output_data,
@@ -202,7 +202,7 @@ def test_poissonian_likelihood_negative_parameters(
     ([[1, 2, 3], [4, 5, 6]], [[0.1, 0.2, 0.3], [0.4, np.nan, 0.6]]),
   ]
 )
-def test_poissonian_likelihood_incompatible_shapes(test_data, test_parameters):
+def test_poissonian_likelihood_negative_parameters(test_data, test_parameters):
     """Tests negative parameter value exception triggers."""
     with pytest.raises(ValueError):
         dkl.build_poissonian_likelihood(test_data, test_parameters)
@@ -215,7 +215,7 @@ def test_poissonian_likelihood_incompatible_shapes(test_data, test_parameters):
     ([1, 2, 3, 4, 5], [1, 2]),
   ]
 )
-def test_poissonian_likelihood_raises(test_data, test_parameters):
+def test_poissonian_likelihood_incompatible_shapes(test_data, test_parameters):
     """Tests build_poissonian_likelihood shape mismatch exception triggers."""
     with pytest.raises(ValueError):
         dkl.build_poissonian_likelihood(test_data, test_parameters)
