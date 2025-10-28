ef9be1b8d9a235595b1ba32034e95b0f9be09ac7
Niko Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2025-10-28 12:55:31 -0400

    Update src/derivkit/__init__.py
    Co-authored-by: Matthijs van der Wild <lonbar@users.noreply.github.com>

--- src/derivkit/__init__.py
+++ src/derivkit/__init__.py
@@ -1,7 +1,7 @@
 """Provides all derivkit methods."""
 
 from derivkit.adaptive.adaptive_fit import AdaptiveFitDerivative
-from derivkit.calculus_kit import CalculusKit  # <-- the KIT is public here
+from derivkit.calculus_kit import CalculusKit
 from derivkit.derivative_kit import DerivativeKit
 from derivkit.finite.finite_difference import FiniteDifferenceDerivative
 from derivkit.forecast_kit import ForecastKit
