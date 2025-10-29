d6bae8cc2303983674118c1bb1869eef20115816
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-10-29 19:05:52 -0400

    chore: sync __init__.py with origin/main

--- src/derivkit/__init__.py
+++ src/derivkit/__init__.py
@@ -1,35 +1,20 @@
 """Provides all derivkit methods."""
 
 from derivkit.adaptive.adaptive_fit import AdaptiveFitDerivative
-from derivkit.derivative_kit import DerivativeKit
+from derivkit.calculus_kit import CalculusKit
+from derivkit.derivative_kit import DerivativeKit, register_method
 from derivkit.finite.finite_difference import FiniteDifferenceDerivative
-from derivkit.forecast_kit import ForecastKit as ForecastKit
-from derivkit.forecasting.expansions import (
-    LikelihoodExpansion as LikelihoodExpansion,
-)
-from derivkit.utils import (
-    central_difference_error_estimate,
-    generate_test_function,
-    is_finite_and_differentiable,
-    is_symmetric_grid,
-    log_debug_message,
-    normalize_derivative,
-)
+from derivkit.forecast_kit import ForecastKit
+from derivkit.forecasting.expansions import LikelihoodExpansion
 
-from derivkit.gaussian_process.gaussian_process import GaussianProcess
-
+DerivativeKit.__module__ = "derivkit.derivative_kit"
 
 __all__ = [
     "AdaptiveFitDerivative",
-    "FiniteDifferenceDerivative",
     "DerivativeKit",
-    "log_debug_message",
-    "is_finite_and_differentiable",
-    "normalize_derivative",
-    "central_difference_error_estimate",
-    "is_symmetric_grid",
-    "generate_test_function",
+    "FiniteDifferenceDerivative",
     "ForecastKit",
     "LikelihoodExpansion",
-    "GaussianProcess"
+    "CalculusKit",
+    "register_method",
 ]
