98cb6697e3670b663886314a4deb0996ebaebfae
C <49960870+ctrendafilova@users.noreply.github.com>
2025-10-29 12:33:02 -0500

    Update src/derivkit/calculus/__init__.py
    Co-authored-by: Matthijs van der Wild <lonbar@users.noreply.github.com>

--- src/derivkit/calculus/__init__.py
+++ src/derivkit/calculus/__init__.py
@@ -3,9 +3,9 @@
 Provides constructors for gradient, Jacobian, and Hessian computations.
 """
 
-from .gradient import build_gradient
-from .hessian import build_hessian
-from .hessian import build_hessian_tensor
-from .jacobian import build_jacobian
+from derivkit.calculus.gradient import build_gradient
+from derivkit.calculus.hessian import build_hessian
+from derivkit.calculus.hessian import build_hessian_tensor
+from derivkit.calculus.jacobian import build_jacobian
 
 __all__ = ["build_gradient", "build_jacobian", "build_hessian"]
