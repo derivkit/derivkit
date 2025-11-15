a60f63d91faa5702186460222d002b371caa6f48
Niko Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2025-11-15 18:47:04 +0000

    Update src/derivkit/local_polynomial_derivative/sampling.py
    Co-authored-by: Matthijs van der Wild <lonbar@users.noreply.github.com>

--- src/derivkit/local_polynomial_derivative/sampling.py
+++ src/derivkit/local_polynomial_derivative/sampling.py
@@ -7,7 +7,7 @@ from typing import Any, Callable
 
 import numpy as np
 
-from .local_poly_config import LocalPolyConfig
+from derivkit.local_polynomial_derivative.local_poly_config import LocalPolyConfig
 
 __all__ = ["build_samples"]
 
