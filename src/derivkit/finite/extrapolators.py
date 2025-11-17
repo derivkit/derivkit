c921df41d6cf6121980db3e6324287a9f13a60e1
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-11-17 21:00:05 +0000

    fixed docstrings in extrapolators fixed gre

--- src/derivkit/finite/extrapolators.py
+++ src/derivkit/finite/extrapolators.py
@@ -332,7 +332,7 @@ def fixed_gre_fd(
     r: float = 2.0,
     return_error: bool = False,
 ) -> NDArray | float | tuple[NDArray | float, NDArray | float]:
-    """Fixed-level Gauss–Richardson extrapolation for finite differences.
+    """Returns a fixed-level Gauss–Richardson extrapolation for finite differences.
 
     Fixed level means we compute m base estimates with step sizes h, h/r, h/r^2, ..., h/r^(m-1)
     and then apply Gauss–Richardson extrapolation to get the final estimate.
