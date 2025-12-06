bed215c1c4c7b3e887540fbbd33ed62ca127a084
Matthijs van der Wild <lonbar@users.noreply.github.com>
2025-12-06 22:17:08 +0000

    Update version dynamically (#272)
    The PR adds dynamic versioning. The simplest way of creating new
    versions is by using the semantic versioning scheme prepended by a `v`
    as described in [the setuptools-scm basic
    usage](https://setuptools-scm.readthedocs.io/en/latest/):
    ```
    git tag v0.1.0
    pip install .
    python
    >>> import derivkit
    >>> derivkit.__version__
    '0.1.0'
    ```
    Users can still create installations at arbitrary commits, which results
    in a fudged version number. For example, moving to commit 0188ce99
    before running the `pip install` command results in a version number of
    `derivkit-0.1.dev854+g0188ce99f`. This should make it a bit easier to
    keep track of different version numbers.
    
    I have also added support for different versions of the documentation.
    Currently each branch and each git tag will result in a separate copy of
    the documentation, which can be accessed through the sidebar.

--- src/derivkit/__init__.py
+++ src/derivkit/__init__.py
@@ -1,11 +1,18 @@
 """Provides all derivkit methods."""
 
+from importlib.metadata import version, PackageNotFoundError
+
 from derivkit.adaptive.adaptive_fit import AdaptiveFitDerivative
 from derivkit.calculus_kit import CalculusKit
 from derivkit.derivative_kit import DerivativeKit, register_method
 from derivkit.finite.finite_difference import FiniteDifferenceDerivative
 from derivkit.forecast_kit import ForecastKit
 from derivkit.forecasting.expansions import LikelihoodExpansion
+
+try:
+    __version__ = version("derivkit")
+except PackageNotFoundError:
+    pass
 
 DerivativeKit.__module__ = "derivkit.derivative_kit"
 
