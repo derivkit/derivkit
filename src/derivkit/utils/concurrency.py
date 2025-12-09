d09a139d4977148645158ee8cd68844ccb118602
niko.sarcevic <nikolina.sarcevic@gmail.com>
2025-12-09 18:27:18 -0500

    added typing into normalize_workers

--- src/derivkit/utils/concurrency.py
+++ src/derivkit/utils/concurrency.py
@@ -137,7 +137,9 @@ def parallel_execute(
             return [worker(*args) for args in arg_tuples]
 
 
-def normalize_workers(n_workers):
+def normalize_workers(
+        n_workers: Any
+) -> int:
     """Ensure n_workers is a positive integer, defaulting to 1.
 
     Args:
