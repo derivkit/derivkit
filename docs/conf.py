0525e33fd0489b0e8bdb927f1db721965dea3364
Niko Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2026-01-04 01:06:32 -0500

    Update docs/conf.py
    Co-authored-by: Matthijs van der Wild <lonbar@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 """Sphinx configuration for the DerivKit documentation."""
 
-from sphinx.ext.doctest import doctest
+from sphinx.ext.doctest import doctest.ELLIPSIS, doctest.NORMALIZE_WHITESPACE
 
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
