1fd6263f8d48cc341b3365e7d4bd6984e42946d4
Matthijs van der Wild <matthijs@vanderwild.com>
2026-02-02 15:41:50 -0500

    Add Fornberg tensor examples and documentation

--- src/derivkit/derivatives/fornberg.py
+++ src/derivkit/derivatives/fornberg.py
@@ -4,8 +4,10 @@ The algorithm was publised by Fornberg in:
 Bengt Fornberg, *Calculation of Weights in Finite Difference Formulas*,
 SIAM Review, vol. 40, No. 3, pp. 685–691, September 1998
 
-Typical usage example:
+Examples:
+=========
 
+Calculating the derivative at a single value::
 >>> import numpy as np
 >>> from derivkit.derivatives.fornberg import FornbergDerivative
 >>> x0 = np.pi/4
@@ -17,6 +19,45 @@ Typical usage example:
 ...     rtol=1e-14,
 ...     atol=0.0,
 ... ))
+True
+
+Calculating the derivative at an array of values using uniform offsets::
+>>> import numpy as np
+>>> from derivkit.derivatives.fornberg import FornbergDerivative
+>>> x0 = np.array([
+...     [[1, 2],
+...      [3, 4]],
+...     [[5, 6],
+...      [7,8]]
+... ])
+>>> grid = np.array([-0.34, -0.02, 0.1, 0.34, 0.98])
+>>> fornberg = FornbergDerivative(lambda x: np.cos(x), x0)
+>>> np.allclose(
+...     fornberg.differentiate(grid=grid, order=1),
+...     -np.sin(x0),
+...     rtol=1e-4,
+...     atol=0.0,
+... )
+True
+
+Calculating the derivative at an array of values using 5 unique offsets for
+each evaluation point::
+>>> import numpy as np
+>>> from derivkit.derivatives.fornberg import FornbergDerivative
+>>> x0 = np.array([2, 7, 10, -np.pi])
+>>> grid = np.array([
+...     [-0.34, -0.02, 0.1, 0.34, 0.98],
+...     [-0.4,  -0.2, -0.1, 0.14, 0.68],
+...     [-0.5,  -0.12, 0.15, 0.64, 0.78],
+...     [-0.1,   0,    0.06, 0.24, 0.8]
+... ]).T
+>>> fornberg = FornbergDerivative(lambda x: np.cos(x), x0)
+>>> np.allclose(
+...     fornberg.differentiate(grid=grid, order=1),
+...     -np.sin(x0),
+...     rtol=1e-4,
+...     atol=1e-4,
+... )
 True
 """
 
@@ -31,9 +72,17 @@ from numpy.typing import NDArray
 class FornbergDerivative:
     """Supplies the Fornberg derivative.
 
+    The Fornberg derivative relies on the interpolation of function values
+    by Lagrange polynomials. For more information see Bengt Fornberg,
+    *Calculation of Weights in Finite Difference Formulas*, SIAM Review,
+    vol. 40, No. 3, pp. 685–691, September 1998.
+
     Attributes:
-        function: the function to be differentiated.
-        x0: the evaluation points for the derivative.
+        function: the function to be differentiated. Must accept a single float
+            and return a float.
+        x0: the evaluation points for the derivative. Must be a float or a
+            structure castable to a Numpy array. If it castable to a Numpy array
+            the function and its derivative will be vectorised over the array.
     """
 
     def __init__(
@@ -44,8 +93,12 @@ class FornbergDerivative:
         """Initialises the class.
 
         Args:
-            function: the function to be differentiated.
-            x0: the evaluation points for the derivative.
+            function: the function to be differentiated. Must accept a single
+                float and return a float.
+            x0: the evaluation points for the derivative. Must be a float or a
+                structure castable to a Numpy array. If it castable to a Numpy
+                array the function and its derivative will be vectorised over
+                the array.
         """
         self.function = function
 
@@ -62,16 +115,49 @@ class FornbergDerivative:
     ) -> NDArray[np.float64]:
         """Constructs the derivative of a given order of a function at a point.
 
-        Currently only the derivative of order ``order`` is returned.
-        An array of orders of ``0`` to ``order`` can be constructed through
-        ``get_weights``. See section 3 of (Fornberg 1998) for more details.
+        The derivative is constructed by recursively differentiating the
+        Lagrange polynomials that approximate the function around the
+        evaluation points.
+
+        Because the derivative of a given order is constructed from the
+        derivatives of lower orders (this is part of the recursion) the
+        method is capable of returning all derivatives up to the specified
+        order. However, currently only the highest derivative is returned.
+
+        See section 3 of (Fornberg 1998) for more details.
 
         Args:
-            grid: a series of points around the evaluation point.
-            order: the order of the derivative.
+            grid: an array of offsets relative to the evaluation points. These
+                points specify the Lagrange interpolation of the function,
+                and are used to calculate the derivatives. The following
+                forms are supported:
+
+                * If ``grid`` is a 1D array it is assumed that it contains
+                  linear offsets from the evaluation points. These
+                  are added uniformly to :data:`FornbergDerivative.x0`.
+
+                * If ``grid`` is an ND array it is assumed that it contains
+                  unique linear offsets for each evaluation point. In this
+                  case the first axis of the grid must correspond to the
+                  offsets while the remaining axes must be equal to the shape
+                  of :data:`FornbergDerivative.x0`. In brief, in this case the
+                  grid must be of shape ``(n, *x0.shape)`` for some integer
+                  ``n``.
+
+                  The advantage of this is that, in this case, the offsets can
+                  be tuned for each evaluation point, although each point must
+                  still be given the same number of offsets.
+
+            order: the order of the derivative. Must be a non-negative number.
+                The case of ``order==0`` corresponds to the Lagrange
+                interpolation of the function.
 
         Returns:
-            The derivative evaluated at the given point.
+            The derivative of :data:`FornbergDerivative.function` evaluated at
+                :data:`FornbergDerivative.x0`.
+
+        Raises:
+            ValueError: if ``order`` is smaller than ``0``.
         """
         if order < 0:
             raise ValueError(
@@ -87,6 +173,8 @@ class FornbergDerivative:
         y = self.function(input_grid)
         weights = np.zeros((*input_grid.shape, order+1), dtype=np.float64)
 
+        # Numpy passes around references to the array data so the weights
+        # are updated in-place. No assignment is necessary.
         self._get_weights(weights, input_grid, order)
         # np.dot contracts the last axis of its first argument with the
         # second to last index of its second argument. The axes are permuted
@@ -109,13 +197,10 @@ class FornbergDerivative:
         """Constructs the weights needed for derivatives up to a given order.
 
         Args:
-            grid: a series of points around the evaluation point.
+            weights: the coefficients for the differentiated Lagrange
+                polynomials. The coefficients are updated in place.
+            grid: a series of offsets around the evaluation points.
             order: the order of the derivative.
-
-        Returns:
-            A 2D array of numbers representing the derivative weights.
-            The 0th axis corresponds to the order of the derivative, with the
-            0th row corresponding with the function itself.
         """
         c1 = 1.0
         c4 = grid[0, :] - self.x0
