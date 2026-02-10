b6f39f37a22332113930eaf850cf708123da8d63
Niko Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2026-02-10 11:38:31 -0500

    Update tests/test_public_api.py
    Co-authored-by: Matthijs van der Wild <lonbar@users.noreply.github.com>

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -23,5 +23,5 @@ def test_public_all_contains_kits():
 
 def test_kits_report_top_level_module():
     """Test that kits present as coming from the top-level package."""
-    for cls in (CalculusKit, DerivativeKit, ForecastKit, LikelihoodKit):
+    for cls in derivkit.SUPPORTED_KITS:
         assert cls.__module__ == "derivkit"
