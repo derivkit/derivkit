ce2811df84f31fbb62e4c439f7fc1336fe2a8fd1
Matthijs van der Wild <matthijs@vanderwild.com>
2026-09-13 16:03:17 -0400

    Improve description of mock derivatives

--- tests/test_forecast_forecast_core.py
+++ tests/test_forecast_forecast_core.py
@@ -607,7 +607,7 @@ def test_dali_symmetries():
     the tensors are invariant under any permutation of the axes. No derivatives are
     calculated.
     """
-    # The "derivative tensors", mimicking a 1D model containing 2 variables.
+    # Mock derivative tensors for a model with one observable and two parameters.
     a = np.array([-1, 3.2])
     b = np.array([[4, 19], [-8, 5]])
     c = np.array([[[0, 1], [3, -3.4]], [[-0.2, -4], [3, -1]]])
